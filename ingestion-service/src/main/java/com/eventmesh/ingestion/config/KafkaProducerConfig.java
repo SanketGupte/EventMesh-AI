32cbc66441527eb56f2a260220ec8540a4d354be
Sanket Gupte <sanket.gupte542@gmail.com>
2026-06-14 07:26:08 +0530

    loggers updated
    changed the loggings to validate the Kafka configurations

--- ingestion-service/src/main/java/com/eventmesh/ingestion/config/KafkaProducerConfig.java
+++ ingestion-service/src/main/java/com/eventmesh/ingestion/config/KafkaProducerConfig.java
@@ -50,10 +50,10 @@ public class KafkaProducerConfig {
             config.put("sasl.jaas.config", saslJaasConfig);
         }
 
-        log.info("Kafka Bootstrap: {}", bootstrapServer);
-        log.info("Kafka Security Protocol: {}", securityProtocol);
-        log.info("Kafka SASL Mechanism: {}", saslMechanism);
-        log.info("Kafka JAAS Config Present: {}", !saslJaasConfig.isBlank());
+        log.info("Kafka Bootstrap={}", bootstrapServers);
+        log.info("Kafka Security Protocol={}", securityProtocol);
+        log.info("Kafka SASL Mechanism={}", saslMechanism);
+        log.info("Kafka JAAS Present={}", saslJaasConfig != null && !saslJaasConfig.isBlank());
 
         return new DefaultKafkaProducerFactory<>(config);
     }
